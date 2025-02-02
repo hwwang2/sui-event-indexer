d7527923142e2dd8188375ec488dda2758c1b61e
hwwang2 <544893492@qq.com>
2025-02-02 19:01:28 +0800

    修改合约地址

--- src/config.ts
+++ src/config.ts
@@ -12,9 +12,9 @@ type Network = "mainnet" | "testnet"
 const network = (process.env.NEXT_PUBLIC_NETWORK as Network) || "testnet";
 
 export const TestnetContract: BaseContract = {
-    packageId: "0x45cb0622b656bd1be77da171287ffc1bf67bc11a90bd3077d6c710703a9a3980",
-    stateId: "0x81144860ff68c1d1f536c98f11baf7525aa64f3fee1c9c97ccd44566892267b7",
-    adminCap: "0x18f81cd19f3c3031da300ada2d29c979af927d61a0305b15dde0f8fccca38846",
+    packageId: "0x90758c3f289dd5dc77b5ba3918a9de298ff13df1668c3411c38bd8543d6c6340",
+    stateId: "0x5586d253829db83ee2ef1f5f82f4217e8461939b8233b6847d065c2ce546644e",
+    adminCap: "0xeaae09dc6de4dacb89c484adc64c4616aa0977edc05c10215e35c0a0ed0b1cf5",
     ownerId: "0x340ff414f778eb4ad4189770a1009ab3d980c1a6cc40832ceb00bf1faa43ad97",
 }
 
